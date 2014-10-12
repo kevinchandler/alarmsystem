59cf571bac385c69aa1d1445f9ddb697cc5e6bc5
Kevin Chandler <im.kevin@me.com>
2014-10-12 13:43:32 -0700

    timezone

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
+  config.time_zone = 'Pacific Time (US & Canada)'
+
   # Configure static asset server for tests with Cache-Control for performance.
   config.serve_static_assets  = true
   config.static_cache_control = 'public, max-age=3600'
