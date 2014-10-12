b76b5a9f821b9a07ee4018f7aa07e13822f42c8a
Kevin Chandler <im.kevin@me.com>
2014-10-12 15:22:55 -0700

    use env var for alert interval

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -2,11 +2,12 @@ class Alarm < ActiveRecord::Base
   has_many :logs
 
   def should_alert?
+    interval = ENV['alarm_notification_interval'].to_i
     if self.state == 'home'
       return false
     elsif self.logs.empty?
       return true
-    elsif self.logs.present? && self.logs.last.created_at < 2.minutes.ago
+    elsif self.logs.present? && self.logs.last.created_at < interval.minutes.ago
       return true
     else
       return false
@@ -17,10 +18,10 @@ class Alarm < ActiveRecord::Base
   def alert
 	# alert the user that motion was detected from a specific alarm
 
-	@twilio_client = Twilio::REST::Client.new ENV['twilio_sid'], ENV['twilio_token']
+    @twilio_client = Twilio::REST::Client.new ENV['twilio_sid'], ENV['twilio_token']
 
-	@twilio_client.account.sms.messages.create(
-	:from => ENV['twilio_phone_number'],
+    @twilio_client.account.sms.messages.create(
+      :from => ENV['twilio_phone_number'],
       :to => ENV['phone_number_to_alert'],
       :body => "Motion Detected at: #{Time.now}. Reply 1 to dismiss"
    )
